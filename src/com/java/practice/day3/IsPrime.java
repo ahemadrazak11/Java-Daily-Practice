1e87fc925737755ec1ef6b0f9e59f819953d51b0
Ahemad Raza Khan <ahemadrazak11@gmail.com>
2026-04-10 19:41:26 +0530

    Day 3 Completed
    In the day 3 I have solved problem to check number is prime or not.

---
+++ src/com/java/practice/day3/IsPrime.java
@@ -0,0 +1,52 @@
+package com.java.practice.day3;
+
+import java.util.Scanner;
+
+public class IsPrime {
+	
+	public static boolean isPrime(int num)
+	{
+		if(num == 2)
+		{
+			return true;
+		}
+		
+		for(int i = 2; i<=Math.sqrt(num); i++)
+		{
+			if(num%i == 0)
+			{
+				return false;
+			}
+		}
+		
+		
+		return true;
+	}
+
+	public static void main(String[] args)
+	{
+		Scanner s1 = new Scanner(System.in);
+		
+		int n = 1;
+		
+		while(n != 0)
+		{
+			System.out.print("Enter the Number: ");
+		    n = s1.nextInt();
+		
+		if(isPrime(n))
+		{
+			System.out.println(n + " : is Prime Number");
+		}
+		else
+		{
+			System.out.println(n + " : is Not A Prime Number");
+		}
+		}
+		
+		
+		s1.close();
+	}
+	
+	
+}
