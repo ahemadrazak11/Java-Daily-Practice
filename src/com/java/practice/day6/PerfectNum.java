77f84309b3cbc08eec33263b7591cd2254ea4319
Ahemad Raza Khan <ahemadrazak11@gmail.com>
2026-04-13 22:43:21 +0530

    Day 6 Completed
    In the day 6 I have solved two problem 1] To find the number is perfect number or not.
    2]  To find the number is perfect square or not.

---
+++ src/com/java/practice/day6/PerfectNum.java
@@ -0,0 +1,34 @@
+package com.java.practice.day6;
+
+import java.util.Scanner;
+
+// the perfect number is nothing but the total sum of it's divisible number excluding itself is equal to itself.
+
+public class PerfectNum {
+
+	public static void main(String[] args)
+	{
+		Scanner s1 = new Scanner(System.in);
+		
+		System.out.print("Enter the number: ");
+		int n = s1.nextInt();
+		int sum = 0;
+		for(int i = 1; i<n; i++)
+		{
+			if(n % i == 0)
+			{
+				sum = sum + i;
+			}
+		}	
+		
+		if(sum == n)
+		{
+			System.out.println("The Number is pefect");
+			
+		}
+		else
+		{
+			System.out.println("The Number is not prefect");
+		}
+	}	
+}
---
+++ src/com/java/practice/day6/PerfectSquare.java
@@ -0,0 +1,27 @@
+package com.java.practice.day6;
+
+import java.util.Scanner;
+
+// the perfect square is nothing but the product of it's INTEGER square root is equal to the number
+
+public class PerfectSquare {
+
+	public static void main(String[] args)
+	{
+		Scanner s1 = new Scanner(System.in);
+		
+		System.out.print("Enter the number: ");
+		int n = s1.nextInt();
+		int root = (int) Math.sqrt(n);	
+		
+		if(root*root == n)
+		{
+			System.out.println("It is a pefect square");
+			
+		}
+		else
+		{
+			System.out.println("It is not a prefect square");
+		}
+	}	
+}
