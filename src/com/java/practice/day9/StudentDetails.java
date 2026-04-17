943efa49dcc4be99ceb756f2d4bfbec5617252e1
ahemadrazak11 <ahemadrazak11@gmail.com>
2026-04-17 22:58:04 +0530

    Add StudentDetails class and .gitignore
    Introduce com.java.practice.day9.StudentDetails: a simple POJO with id, name, age, mobile and a static college field, a constructor, and PrintDetails() to print instance info. main() demonstrates usage by creating two students. Also add .gitignore to exclude the generated StudentDetails.class file.

---
+++ src/com/java/practice/day9/StudentDetails.java
@@ -0,0 +1,42 @@
+package com.java.practice.day9;
+
+public class StudentDetails {
+
+	int id;
+	String name;
+	int age;
+	long mobile;
+	static String college = "QSpider";
+	
+	public StudentDetails(int id, String name, int age, long mobile)
+	{
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.mobile = mobile;
+	}
+	
+	public void PrintDetails()
+	{
+		
+		System.out.println("ID: " + id);
+		System.out.println("Name: " + name);
+		System.out.print("Age: " + age);
+		System.out.println("Mobile: " + mobile);
+		System.out.println("College: " + college);
+		System.out.println("____________________");
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		StudentDetails s1 = new StudentDetails(101, "Student1", 20, 9999);
+		StudentDetails s2 = new StudentDetails(102, "Student2", 21, 8888);
+		
+		s1.PrintDetails();
+		s2.PrintDetails();
+		
+				
+	}
+	
+}
