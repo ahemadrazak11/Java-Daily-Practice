e007eaa7c0852539e34ea444dc6b31afb7d239f6
ahemadrazak11 <ahemadrazak11@gmail.com>
2026-05-06 21:05:25 +0530

    day 22 completed
    Add new EqualsMethod.java defining a simple class with id and name fields and a constructor. The equals(Object O) method is partially implemented and currently invalid (directly accesses O.id and has an unfinished conditional), so it will not compile; it needs proper null/type checks, casting, an @Override annotation and a complete equality check. File added under package com.java.practice.day22.

---
+++ src/com/java/practice/day22/EqualsMethod.java
@@ -0,0 +1,18 @@
+package com.java.practice.day22;
+
+public class EqualsMethod {
+
+	int id;
+	String name;
+	public EqualsMethod(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	
+	public boolean equals(Object O)
+	{
+		if((this.id == O.id) && )
+	}
+	
+}
