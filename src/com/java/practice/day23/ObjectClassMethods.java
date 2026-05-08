d85d194fb777c79adc1c3e545e58932724074b38
ahemadrazak11 <ahemadrazak11@gmail.com>
2026-05-08 21:20:32 +0530

    day 23 completed
    Add ObjectClassMethods.java demonstrating a simple Java object with id and name fields, constructors, and an overridden equals(Object) method plus a main() that tests equality. Also add a .gitignore in the package to ignore the compiled ObjectClassMethods.class. Note: toString and hashCode overrides are left as placeholders in the file.

---
+++ src/com/java/practice/day23/ObjectClassMethods.java
@@ -0,0 +1,48 @@
+package com.java.practice.day23;
+
+public class ObjectClassMethods {
+
+	int id;
+	String name;
+	
+	public ObjectClassMethods()
+	{
+		
+	}
+	
+	
+	public ObjectClassMethods(int id, String name)
+	{
+		this.id = id;
+		this.name = name;
+	}
+	
+	//toString method overriding
+	
+	
+	
+	
+	//equals method overriding
+	public boolean equals(Object o)
+	{
+		ObjectClassMethods s = (ObjectClassMethods) o;
+		
+		if(this.id == s.id && this.name.equals(s.name))
+		{
+			return true;
+		}
+		return false;
+	}
+	
+	//hashCode method overriding
+	
+
+	public static void main(String[] args)
+	{
+		ObjectClassMethods Obj1 = new ObjectClassMethods(101, "Ahemad");
+		
+		ObjectClassMethods Obj2 =new ObjectClassMethods(102, "Ahemad");
+		
+		System.out.println(Obj2.equals(Obj1)); // false
+	}
+}
