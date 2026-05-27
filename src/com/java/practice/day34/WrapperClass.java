b6b857a3cc8711678d8e18f711a5a8ce2d8c719d
ahemadrazak11 <ahemadrazak11@gmail.com>
2026-05-27 22:29:35 +0530

    day 34 completed
    Introduce WrapperClass.java that demonstrates Java wrapper types (Byte, Short, Integer, Long, Float, Double) by assigning sample values and printing them. Add package .gitignore to ignore the generated WrapperClass.class file.

---
+++ src/com/java/practice/day34/WrapperClass.java
@@ -0,0 +1,35 @@
+package com.java.practice.day34;
+
+public class WrapperClass {
+
+	public static void main(String[] args) {
+		
+		Byte byt = 10;
+		
+		Short shrt = 30;
+		
+		Integer intgr = 100;
+		
+		Long lng = 10L;
+		
+		Float flt = 200f;
+		
+		Double dbl = 200.12;
+		
+		
+		System.out.println("Byte: " + byt);
+		
+		System.out.println("Short: " + shrt);
+		
+		System.out.println("Integer: " + intgr);
+		
+		System.out.println("Long: " + lng);
+		
+		System.out.println("Float: " + flt);
+		
+		System.out.println("Double: " + dbl);
+		
+		
+		
+	}
+}
