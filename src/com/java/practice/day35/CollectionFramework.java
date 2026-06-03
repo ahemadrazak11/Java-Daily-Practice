de02f0cffff8c4877971b1754282869e1a68f47f
ahemadrazak11 <ahemadrazak11@gmail.com>
2026-06-03 11:24:39 +0530

    day 35 completed
    Add CollectionFramework.java with a simple main that demonstrates Integer autoboxing and Integer.equals against a primitive int, printing the comparison result. Also add a .gitignore to ignore the generated CollectionFramework.class file.

---
+++ src/com/java/practice/day35/CollectionFramework.java
@@ -0,0 +1,13 @@
+package com.java.practice.day35;
+
+public class CollectionFramework {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		
+		Integer i = 10;
+		
+		System.out.println(i.equals(a));	
+	}
+}
