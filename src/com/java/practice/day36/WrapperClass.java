25c57be03eb46ff23d256889f748c85d0d9aa886
ahemadrazak11 <ahemadrazak11@gmail.com>
2026-06-05 23:03:53 +0530

    day 36 completed
    Replace deprecated new Integer(20) with Integer.valueOf(20) and add an explicit unboxing example (Integer x = 20; int y = x). Also add a clarifying comment about implicit unboxing on the earlier print statement and minor formatting adjustments.

--- src/com/java/practice/day36/WrapperClass.java
+++ src/com/java/practice/day36/WrapperClass.java
@@ -11,11 +11,14 @@ public class WrapperClass {
 			
 			
 			System.out.println(a);
-			System.out.println(b);
+			System.out.println(b); // unboxing happen here because java does add intValue() method implicitly.
 			
 			
-			Integer ab = new Integer(20); // it is depricated means it does not uses in new java. but it is valid.
+			Integer ab = Integer.valueOf(20); // it is depricated means it does not uses in new java. but it is valid.
 			
+			
+			Integer x = 20;
+			int y = x ; // unboxing
 		}
 		
 }
