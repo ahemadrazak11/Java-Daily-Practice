b16e751370f3aafebfdd537cdb236247a7da3874
ahemadrazak11 <ahemadrazak11@gmail.com>
2026-06-06 11:15:34 +0530

    Update WrapperClass examples and .gitignore
    Add .gitignore for day32 and extend day36 .gitignore to ignore Test.class; modify WrapperClass.java by commenting out direct prints, add examples demonstrating wrapper immutability, Integer caching behavior, and Character utility method usage (isDigit/isLetter/isLetterOrDigit). Small cleanup and additional illustrative comments for autoboxing/unboxing.

--- src/com/java/practice/day36/WrapperClass.java
+++ src/com/java/practice/day36/WrapperClass.java
@@ -10,8 +10,8 @@ public class WrapperClass {
 			Integer b = a; // Autoboxing happen here.  Primitive ---> Integer Wrapper Class
 			
 			
-			System.out.println(a);
-			System.out.println(b); // unboxing happen here because java does add intValue() method implicitly.
+			//System.out.println(a);
+			//System.out.println(b); // unboxing happen here because java does add intValue() method implicitly.
 			
 			
 			Integer ab = Integer.valueOf(20); // it is depricated means it does not uses in new java. but it is valid.
@@ -19,6 +19,42 @@ public class WrapperClass {
 			
 			Integer x = 20;
 			int y = x ; // unboxing
+			
+			
+			// Characteristics of Wrapper Classes.
+			// 1. Wrapper class immutable.
+			
+			Integer i = 20;
+			
+			//System.out.println(System.identityHashCode(i));
+			
+			i = 50; // 
+			
+			//System.out.println(System.identityHashCode(i));
+			
+			Integer az = 200;
+			Integer bz = 200;
+			
+			//System.out.println(az == bz);	// false
+			
+			
+			Integer cz = 100;
+			Integer dz = 100;
+			
+			//System.out.println(cz == dz); // true
+			
+			Integer num = 100;
+			
+			//System.out.println(num);
+			
+			num = 200;
+			//System.out.println(num);
+			
+			
+			System.out.println(Character.isDigit('5'));
+			System.out.println(Character.isLetter('A'));
+			System.out.println(Character.isLetterOrDigit('.'));
+
 		}
 		
 }
