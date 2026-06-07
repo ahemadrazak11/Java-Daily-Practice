2c6e4e37f9f02f99b6da061151b707df65e9e1e5
ahemadrazak11 <ahemadrazak11@gmail.com>
2026-06-07 20:34:03 +0530

    Add main demo to BooleanClass
    Introduce a main method in BooleanClass to demonstrate Boolean wrapper usage: autoboxing, Boolean.valueOf, booleanValue, Boolean.parseBoolean, Boolean.compare, and toString with example print statements for learning/practice.

--- src/com/java/practice/day38/BooleanClass.java
+++ src/com/java/practice/day38/BooleanClass.java
@@ -2,4 +2,22 @@ package com.java.practice.day38;
 
 public class BooleanClass {
 
+	public static void main(String[] args) {
+		
+		Boolean bln = true; // internally java add Boolean.valueOf(true);
+		
+		System.out.println(bln);
+		
+		Boolean bln1 = Boolean.valueOf(false);
+		
+		System.out.println(bln1);
+		
+		System.out.println(bln.booleanValue()); // convert boolean object to primitve;
+		
+		System.out.println(Boolean.parseBoolean("true")); // String to primitive.
+		
+		System.out.println(Boolean.compare(false, true)); // return values like -1 0 1
+		
+		System.out.println("false as String: " + bln1.toString());
+	}
 }
