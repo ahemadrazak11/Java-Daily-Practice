72816fd5455ccf6206ba9cfc797f02234cce837b
ahemadrazak11 <ahemadrazak11@gmail.com>
2026-06-07 12:44:08 +0530

    Day38 Completed.
    Add main method examples demonstrating Character and Double wrapper class methods in CharachterClass.java and DoubleClass.java (e.g. isDigit/isLetter/isUpperCase/toUpperCase, parseDouble/doubleValue/compare/sum/min/max/toString). Also add a .gitignore to exclude compiled .class files (BooleanClass.class, CharachterClass.class, DoubleClass.class). These changes provide simple educational demos of Java wrapper APIs and keep build artifacts out of source control.

--- src/com/java/practice/day38/CharachterClass.java
+++ src/com/java/practice/day38/CharachterClass.java
@@ -2,4 +2,30 @@ package com.java.practice.day38;
 
 public class CharachterClass {
 
+public static void main(String[] args) {
+		
+		System.out.println("Character Wrapper Class Methods Example,");
+		
+		Character ch = Character.valueOf('A'); // primitive value assigned to Character Object.
+	
+		System.out.println(Character.isDigit('1'));
+		
+		System.out.println(Character.isLetter('S'));
+		
+		System.out.println(Character.isLetterOrDigit('>'));
+		
+		System.out.println(Character.isUpperCase('A'));
+		
+		System.out.println(Character.isLowerCase('c'));
+				
+		System.out.println(Character.isWhitespace(' '));
+		
+		System.out.println(Character.toUpperCase('a'));
+		
+		System.out.println(Character.toLowerCase('F'));
+		
+		
+		
+	}
+
 }
--- src/com/java/practice/day38/DoubleClass.java
+++ src/com/java/practice/day38/DoubleClass.java
@@ -2,4 +2,31 @@ package com.java.practice.day38;
 
 public class DoubleClass {
 
+	public static void main(String[] args) {
+		
+		System.out.println("Double Wrapper Class Methods Example,");
+		
+		Double num = Double.valueOf(100.0); // primitive value assigned to Double Object.
+		
+		System.out.println(Double.parseDouble("1005.0"));  // converting string into primitive.
+		
+		System.out.println(num.doubleValue()); // converting Double object into primitive.
+		
+		System.out.println(num.equals(100.0)); // compare two values.
+		
+		System.out.println(num.compareTo(20.0)); // compare two values.
+		
+		System.out.println(Double.compare(100.0, 10.0)); // compare two value static method.
+		
+		System.out.println(Double.sum(10, 20));
+		
+		System.out.println(Double.min(20, 10.0));
+		
+		System.out.println(Double.max(200.0, 10.02));
+		
+		System.out.println(num.toString());
+		
+		
+		
+	}
 }
