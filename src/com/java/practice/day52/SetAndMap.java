830e52bc50ce204e57693f94620935b9e35d84e9
ahemadrazak11 <ahemadrazak11@gmail.com>
2026-08-07 00:22:06 +0530

    Set and Map Added.

---
+++ src/com/java/practice/day52/SetAndMap.java
@@ -0,0 +1,36 @@
+package com.java.practice.day52;
+
+import java.util.*;
+
+public class SetAndMap {
+	
+	public static void main(String[] args) {
+		
+		
+		Set<String> str = new HashSet<>();
+		
+		str.add("Ahemad");
+		str.add("Raza");
+		str.add("Khan");
+		str.add("A");
+		str.add("B");
+		str.add("Z");
+		str.add("ZZ");
+		
+		for(String s : str) {
+			System.out.println(s);
+		}
+		
+		
+		
+		Map<Integer, String> i = new HashMap<>();
+		
+		i.put(1, "Ahemad");
+		i.put(2, "Raza");
+		
+		System.out.println(i);
+		
+}
+	
+}
+	
