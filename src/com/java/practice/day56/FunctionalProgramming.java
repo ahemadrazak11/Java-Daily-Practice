20335551c9f45aca4277bf3dbac8ba1c7b4c009e
ahemadrazak11 <ahemadrazak11@gmail.com>
2026-09-06 23:23:40 +0530

    Functional Interface and Lamda Expression Added

--- src/com/java/practice/day56/FunctionalProgramming.java
+++ src/com/java/practice/day56/FunctionalProgramming.java
@@ -2,4 +2,21 @@ package com.java.practice.day56;
 
 public class FunctionalProgramming {
 
+	public static void main(String[] args) {
+
+        Calculator add = (a, b) -> a + b;
+        Calculator multiply = (a, b) -> a * b;
+        Calculator subtract = (a, b) -> a - b;
+
+        System.out.println("Addition: " + add.calculate(10, 5));
+        System.out.println("Multiplication: " + multiply.calculate(10, 5));
+        System.out.println("Subtraction: " + subtract.calculate(10, 5));
+    }
+	
+}
+
+
+@FunctionalInterface
+interface Calculator {
+    int calculate(int a, int b);
 }
