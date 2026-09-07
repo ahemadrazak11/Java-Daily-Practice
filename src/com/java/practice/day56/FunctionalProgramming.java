e23c6dd55c5657faca9e1a8a3f0cd6a9c721a2a0
ahemadrazak11 <ahemadrazak11@gmail.com>
2026-09-07 23:55:50 +0530

    Division Operation Added.

--- src/com/java/practice/day56/FunctionalProgramming.java
+++ src/com/java/practice/day56/FunctionalProgramming.java
@@ -15,10 +15,17 @@ public class FunctionalProgramming {
         
         // this is subtraction operation using lambda expression
         Calculator subtract = (a, b) -> a - b;
-
+        
+     // this is Division operation using lambda expression
+        Calculator div = (a, b) -> a / b;
+        
+        
+        System.out.println("A = " + 10 +" " + "B = " + 5);
         System.out.println("Addition: " + add.calculate(10, 5));
         System.out.println("Multiplication: " + multiply.calculate(10, 5));
         System.out.println("Subtraction: " + subtract.calculate(10, 5));
+        System.out.println("Division: " + div.calculate(10, 5));
+        
     }
 	
 }
