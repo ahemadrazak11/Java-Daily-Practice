73bd04ba3d2344252161d6a50a59d1c9197d779f
ahemadrazak11 <ahemadrazak11@gmail.com>
2026-09-12 00:02:39 +0530

    Static Method Reference Added.

--- src/com/java/practice/day56/MethodReference.java
+++ src/com/java/practice/day56/MethodReference.java
@@ -2,11 +2,49 @@ package com.java.practice.day56;
 
 import java.util.*;
 
+import java.util.function.Function;
+
 public class MethodReference {
+	
+	
+	public static int sqaure(int x) {
+		
+		return x * x;
+	}
 	
 	public static void main(String[] args) {
 		
-		List<Integer> list = new ArrayList<>(List.of(10,50,30,90,40,60,70,20,80));
+		
+		//1. Reference to a Static Method
+		
+		
+		// Lambda Expression
+		Function<Integer, Integer> f1 = x -> MethodReference.sqaure(x);
+		
+		System.out.println("Square By Lambda Expression = " + f1.apply(5));
+		
+		// Method Reference
+		
+		Function<Integer, Integer> f2 = MethodReference::sqaure;
+		
+		System.out.println("Sqaure by Method Reference = " + f2.apply(10));
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*
+		 List<Integer> list = new ArrayList<>(List.of(10,50,30,90,40,60,70,20,80));
 		
 		Collections.sort(list, (l1, l2)-> l1 - l2); // the sort method accept list and one is Comaprator ka object.
 		
@@ -21,7 +59,10 @@ public class MethodReference {
 		list.forEach(System.out::println);
 		
 		
+		*/
+		
 		
 	}
 
 }
+
