865e419e84a9973c5277649e3ecb78e2f33a87ea
ahemadrazak11 <ahemadrazak11@gmail.com>
2026-09-14 01:10:12 +0530

    Stream API added

--- src/com/java/practice/day57/IntroductionToStream.java
+++ src/com/java/practice/day57/IntroductionToStream.java
@@ -1,5 +1,45 @@
 package com.java.practice.day57;
 
+import java.util.*;
+import java.util.stream.Stream;
+
 public class IntroductionToStream {
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		List<Integer> list = new ArrayList<>(List.of(10, 20, 30, 40, 50, 60));
+		
+		
+		
+		// ways to use Stream
+		
+		//1 Collection
+		
+		 list.stream().filter(x -> x > 20).map(x -> x * 2).forEach(System.out::println);
+		 
+		 System.out.println("==================================================");
+		 
+		 
+		 
+		 //2 Collection
+		 Stream<Integer> s = list.stream()
+				 						.filter(x -> x > 20)
+				 						.map(x -> x * 3);
+				 						
+				 		s.forEach(System.out::println);
+		
+		  //3 Array
+				 		
+		 String[] str = {"A"
+				 		
+				 		
+		
+	}
+	
+	
 
 }
