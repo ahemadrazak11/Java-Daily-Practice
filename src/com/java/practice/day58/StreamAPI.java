30c8e0df337328a1b1d95a4bd5d4ad19949c35d3
ahemadrazak11 <ahemadrazak11@gmail.com>
2026-09-14 23:39:35 +0530

    Some Question Solved by Stream API.
    Stream API is a feature introduced in a java 8 that provide  a way to process the collections data in a sequence of operations like filtering, searching, sorting, mapping etc.

--- src/com/java/practice/day58/StreamAPI.java
+++ src/com/java/practice/day58/StreamAPI.java
@@ -1,5 +1,60 @@
 package com.java.practice.day58;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class StreamAPI {
+	
+	public static void main(String[] args) {
+		
+		List<Integer> list = new ArrayList<Integer>(List.of(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20));
+		
+		// 1. Find Even Number by stream
+		
+		System.out.println("==================	Even	=======================");
+		
+		List<Integer> evenList = list.stream()
+									 .filter(x -> x % 2 == 0)
+									 .toList();
+		
+					 evenList.forEach(System.out::println);
+					 
+		// 2. Find Odd number by stream
+		System.out.println("====================	Odd	=====================");
+					 
+		Stream<Integer> oddStream = list.stream()
+										.filter(x -> x % 2 != 0);
+		
+						oddStream.forEach(System.out::println); 
+						//here we used the stream and stream get closed means we can not use it again if use then get Exception.
+						
+						//oddStream.forEach(System.out::println); // through an exception
+		
+		System.out.println("=======================	Square ==================");
+		
+		List<Integer> getSquare = list.stream()
+									  .map(x -> x * x)
+									  .toList();
+		
+		getSquare.forEach(System.out::println);
+		
+		
+		System.out.println("=======================	Reverse List Assigned to another list ==================");
+		
+		
+		List<Integer> reverseList = List.copyOf(list.reversed());
+		
+		reverseList.forEach(System.out::println);
+		
+		
+		List<Integer> sortedList = reverseList.stream()
+											  .sorted()
+											  .toList();
+		
+		System.out.println("=======================	Sorted List By Stream ==================");
+		
+					sortedList.forEach(System.out::println);
+	}
 
 }
